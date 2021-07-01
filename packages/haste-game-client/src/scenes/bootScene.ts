4a2ee2ce0461ab1997c9b9ecd9cf282db4457e22
Keith LaForce <klaforce@gmail.com>
2021-07-01 06:57:04 -0400

    chore(global): tweaks after pnpm update

--- packages/haste-game-client/src/scenes/bootScene.ts
+++ packages/haste-game-client/src/scenes/bootScene.ts
@@ -63,26 +63,40 @@ export class BootScene extends Phaser.Scene {
   update() {
     if (this.isAuthenticated !== undefined && this.loginButton === undefined) {
       if (!this.isAuthenticated) {
-        this.loginButton = new Button(this, 50, 25, 'Login', { fill: '#f00' }, async (): Promise<void> => {
-          return this.login();
-        });
+        this.loginButton = new Button(
+          this,
+          50,
+          25,
+          'Login',
+          { fill: '#f00' },
+          async (): Promise<void> => {
+            return this.login();
+          },
+        );
         this.add.existing(this.loginButton);
       } else {
         if (this.startButton === undefined) {
           const hasteGame = this.game as HasteGame;
 
-          this.startButton = new Button(this, 50, 25, 'Start', { fill: '#f00' }, (): Promise<void> => {
-            hasteGame.socketManager.gameInitCompletedEvent.on((data: HasteGameState) => {
-              hasteGame.state = data;
-              // once a user is authenticated and hits the start button
-              // transition to the main game. Pass in auth0 to support the logout
-              // button
-              this.scene.start('GameScene', { auth: this.auth0 } as GameSceneData);
-            });
+          this.startButton = new Button(
+            this,
+            50,
+            25,
+            'Start',
+            { fill: '#f00' },
+            (): Promise<void> => {
+              hasteGame.socketManager.gameInitCompletedEvent.on((data: HasteGameState) => {
+                hasteGame.state = data;
+                // once a user is authenticated and hits the start button
+                // transition to the main game. Pass in auth0 to support the logout
+                // button
+                this.scene.start('GameScene', { auth: this.auth0 } as GameSceneData);
+              });
 
-            hasteGame.socketManager.gameInitEvent.emit();
-            return Promise.resolve();
-          });
+              hasteGame.socketManager.gameInitEvent.emit();
+              return Promise.resolve();
+            },
+          );
           this.add.existing(this.startButton);
         }
       }
