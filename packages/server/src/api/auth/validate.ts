0fb5c2d4a9ff487b39879f1a04d4fb82335ab0d3
Eric LaForce <elaforc@gmail.com>
2021-09-26 06:25:06 -0400

    fix(server): updated error messages

--- packages/server/src/api/auth/validate.ts
+++ packages/server/src/api/auth/validate.ts
@@ -28,17 +28,23 @@ export const validateAuthenticationToken = (playerAuthToken: string, authUrl: st
         if (err)
           return reject(
             new Error(
-              `The token does not match the authentication configuration. Try validating that you are utilizing the correct auth url for Haste.authenticate()`,
+              `The token does not match the authentication configuration. Validate that you are utilizing the correct auth url for Haste.authenticate()`,
             ),
           );
         if (decoded.iss === auth0Url && decoded.exp > new Date().getTime() / 1000) {
           return resolve(decoded);
         } else {
-          return reject(new Error('The player access token provided was invalid.'));
+          return reject(
+            new Error('The player access token provided was invalid. Ensure that you are logged in correctly.'),
+          );
         }
       });
     } else {
-      reject(new Error('A player token must be provided to the middleware.'));
+      reject(
+        new Error(
+          'A player token must be provided to the middleware. Verify the token is being supplied and the user is logged in correctly',
+        ),
+      );
     }
   });
 };
