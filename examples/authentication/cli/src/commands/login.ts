cb1d6eeb18fafbcdbb9d9b64bde365b70320c930
Eric LaForce <elaforc@gmail.com>
2021-12-07 19:54:05 -0500

    docs(example): completed cli login example

--- examples/authentication/cli/src/commands/login.ts
+++ examples/authentication/cli/src/commands/login.ts
@@ -3,6 +3,13 @@ import os from 'os';
 import axios from 'axios';
 import { config } from 'dotenv';
 import BaseCommand from '../common/base-command';
+import Netrc from 'netrc-parser';
+import jwtDecode, { JwtPayload } from 'jwt-decode';
+
+interface NetrcEntry {
+  login: string;
+  password: string;
+}
 
 export default class Login extends BaseCommand {
   static description = 'Login to Haste Arcade';
@@ -13,14 +20,84 @@ export default class Login extends BaseCommand {
     help: flags.help({ char: 'h' }),
   };
 
+  async sleep(ms: number) {
+    // eslint-disable-next-line no-promise-executor-return
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async fetchAuth(
+    retries: number,
+    response: { data: { cliUrl: string; token: string; requestorId: string } },
+    loginSpinner: { fail: (arg0: string) => void },
+  ): Promise<any> {
+    try {
+      const accessTokenResponse = await axios.get<{ access_token: string }>(
+        `${process.env.AUTH_API_URL}${response.data.cliUrl}/${response.data.requestorId}`,
+        {
+          headers: { authorization: `Bearer ${response.data.token}` },
+        },
+      );
+
+      const decoded = jwtDecode<JwtPayload & { 'https://hastearcade.com/email': string }>(
+        accessTokenResponse.data.access_token,
+      );
+      const email = decoded['https://hastearcade.com/email'];
+
+      return {
+        login: email,
+        password: accessTokenResponse.data.access_token,
+      } as NetrcEntry;
+    } catch (error) {
+      if (retries > 0 && error.response.status >= 400) {
+        await this.sleep(10_000);
+        return this.fetchAuth(retries - 1, response, loginSpinner);
+      }
+
+      loginSpinner.fail(`The login was not completed successfully. Please try again. ${error}`);
+      throw error;
+    }
+  }
+
   async run(): Promise<void> {
     config({ path: `${__dirname}/.env` });
+
+    await Netrc.load();
+    await this.removeToken();
+
     const response = await axios.post(`${process.env.AUTH_API_URL}/cli`, { description: os.hostname() }, {});
     this.logCommandStart(
       `Please open the following link in your browser to login: ${process.env.AUTH_CLIENT_URL}${response.data.browserUrl}`,
     );
 
-    // const succeedSpinner = this.spinner.add('Once you login with your browser, the login will complete');
-    // const failSpinner = this.spinner.add('If the login fails, this will show');
+    const loginSpinner = this.spinner.add('Waiting for you to login within your browser using the link above');
+
+    await this.sleep(10_000);
+    const auth = await this.fetchAuth(3, response, loginSpinner);
+    this.saveToken(auth);
+    loginSpinner.succeed('You are successfully logged in.');
+  }
+
+  private async saveToken(entry: NetrcEntry) {
+    const hosts = ['api.hastearcade.com'];
+
+    hosts.forEach((host) => {
+      if (!Netrc.machines[host]) Netrc.machines[host] = {};
+      Netrc.machines[host].login = entry.login;
+      Netrc.machines[host].password = entry.password;
+      delete Netrc.machines[host].method;
+      delete Netrc.machines[host].org;
+    });
+
+    await Netrc.save();
+  }
+
+  private async removeToken() {
+    const hosts = ['api.hastearcade.com'];
+
+    hosts.forEach((host) => {
+      Netrc.machines[host] = {};
+    });
+
+    await Netrc.save();
   }
 }
